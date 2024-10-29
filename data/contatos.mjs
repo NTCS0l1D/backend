c93e478128dc0998ef77bf63c27b41f8362ababc
git config --global user.name FIRST_NAME <123yuri321@gmail.com>
2024-10-28 21:48:34 -0300

    first commit

---
+++ data/contatos.mjs
@@ -0,0 +1,18 @@
+const contatos = [
+    {
+        id: 1,
+        nome: 'Yuri',
+        genero: "Masculino",
+        telefone: '123456789',
+        email: 'yuri.a.araujo@iesb.edu.br'
+    },
+    {
+        id: 2,
+        nome: 'Marcela',
+        genero: "Feminino",
+        telefone: '987654321',
+        email: 'marcela@gmail.com'
+    }
+];
+
+export default contatos;
